998d4b792571e0ddb9e57c6bafe99efee9cbc9be
yohanastg12 <112596583+yohanastg12@users.noreply.github.com>
2025-11-11 18:04:40 +0700

    memasukkan semua proyek ke repo

--- demo/kgrs.py
+++ demo/kgrs.py
@@ -1,17 +1,32 @@
 import os
+import sys
 from typing import List
 import numpy as np
 import torch
 import random
 from tqdm import tqdm
 
+# os.path.join(..., '..') -> go up one level to (.../KG-BASED_RECOMM...)
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+)
+
 
 class Dataloader:
-    def __init__(self, train_pos, train_neg, kg_lines, train_batch_size: int = 128, neg_rate: float = 2):
+    def __init__(
+        self,
+        train_pos,
+        train_neg,
+        kg_lines,
+        n_user,
+        n_item,
+        train_batch_size: int = 128,
+        neg_rate: float = 2,
+    ):
         self.kg, self.rel_dict, self.n_entity = self._convert_kg(kg_lines)
         self.train_pos, self.train_neg = train_pos, train_neg
-        self.n_user = max(list(set(self.train_pos[:, 0]) | set(self.train_neg[:, 0]))) + 1
-        self.n_item = max(list(set(self.train_pos[:, 1]) | set(self.train_neg[:, 1]))) + 1
+        self.n_user = n_user
+        self.n_item = n_item
         self._load_ratings()
         self.known_neg_dict = []  # Save the currently known negative samples
         self._add_recsys_to_kg()
@@ -22,7 +37,9 @@ class Dataloader:
 
     def _add_recsys_to_kg(self):
         # Add the interaction data to the kg as the extra relation
-        self.rel_dict['feedback_recsys'] = max([self.rel_dict[key] for key in self.rel_dict]) + 1
+        self.rel_dict['feedback_recsys'] = (
+            max([self.rel_dict[key] for key in self.rel_dict]) + 1
+        )
         for interaction in self.train_pos:
             self.kg.append((interaction[0], self.rel_dict['feedback_recsys'], interaction[1]))
         for interaction in self.train_neg:
@@ -41,7 +58,7 @@ class Dataloader:
         entity_set = set()
         kg = []
         rel_dict = {}
-        for line in open('./relation2id.txt', encoding='utf8').readlines():
+        for line in open('../data/relation2id.txt', encoding='utf8').readlines():
             elements = line.replace('\n', '').split('\t')
             rel_dict[elements[0]] = int(elements[1])
         for line in lines:
@@ -52,7 +69,6 @@ class Dataloader:
             kg.append((head, relation, tail))
             entity_set.add(head)
             entity_set.add(tail)
-
         print('number of entities (containing items): %d' % len(entity_set))
         print('number of relations: %d' % len(rel_dict))
         return kg, rel_dict, max(list(entity_set)) + 1 if len(entity_set) > 0 else 0
@@ -81,6 +97,7 @@ class Dataloader:
                 rt_head_set[(fact[1], fact[2])] = set()
             hr_tail_set[(fact[0], fact[1])].add(fact[2])
             rt_head_set[(fact[1], fact[2])].add(fact[0])
+
         sample_failed_time = 0
         sample_failed_max = len(self.kg) * self.neg_rate
         # Sample extra negative training samples
@@ -118,6 +135,7 @@ class Dataloader:
                         if sample_failed_time < sample_failed_max:
                             rt_head_set[(fact[1], fact[2])].add(head)
                             neg_data.append((head, fact[1], fact[2]))
+
         random.shuffle(pos_data)
         random.shuffle(neg_data)
         pos_batches = np.array_split(pos_data, max(1, len(pos_data) // self.train_batch_size))
@@ -128,8 +146,18 @@ class Dataloader:
 
 
 class TransE(torch.nn.Module):
-    def __init__(self, ent_num: int, rel_num: int, dataloader: Dataloader, dim: int = 64, l1: bool = True,
-                 margin: float = 1, learning_rate: float = 1e-3, weight_decay: float = 1e-4, device_index: int = 0):
+    def __init__(
+        self,
+        ent_num: int,
+        rel_num: int,
+        dataloader: Dataloader,
+        dim: int = 64,
+        l1: bool = True,
+        margin: float = 1,
+        learning_rate: float = 1e-3,
+        weight_decay: float = 1e-4,
+        device_index: int = 0,
+    ):
         super().__init__()
         self.device = torch.device('cuda:{}'.format(device_index)) if device_index >= 0 else torch.device('cpu')
         self.ent_num: int = ent_num
@@ -151,8 +179,7 @@ class TransE(torch.nn.Module):
         if self.l1:
             score = torch.sum(torch.abs(torch.subtract(torch.add(head_emb, rel_emb), tail_emb)), dim=-1, keepdim=True)
         else:
-            score = torch.sum(torch.square(torch.subtract(torch.add(head_emb, rel_emb), tail_emb)), dim=-1,
-                              keepdim=True)
+            score = torch.sum(torch.square(torch.subtract(torch.add(head_emb, rel_emb), tail_emb)), dim=-1, keepdim=True)
         return -score
 
     def optimize(self, pos, neg):
@@ -216,30 +243,28 @@ class TransE(torch.nn.Module):
 
 
 class KGRS:
-    def __init__(self, train_pos: np.array, train_neg: np.array, kg_lines: List[str]):
+    def __init__(self, train_pos: np.array, train_neg: np.array, kg_lines: List[str], n_user: int, n_item=int):
         """
         Initialize the Algorithm
         :param train_pos: The Positive Samples in the Training Set, is a numpy matrix with shape (n,3),
-                          while `n` is the number of positive samples, and in each sample, the first
-                          number represent the user, the second represent the item, and the last indicate
-                          interest or not. e.g. [[1,2,1], [2,5,1],[1,3,1]] indicate that user 1 has
-                          interest in item 2 and item 3, user 2 has interest in item 5.
+        while n is the number of positive samples, and in each sample, the first number represent the user, the second represent the item, and the last indicate interest or not. e.g. [[1,2,1], [2,5,1],[1,3,1]] indicate that user 1 has interest in item 2 and item 3, user 2 has interest in item 5.
         :param train_neg: The Negative Samples in the Training Set, is a numpy matrix with shape (n,3),
-                          while `n` is the number of positive samples, and in each sample, the first
-                          number represent the user, the second represent the item, and the last indicate
-                           interest or not. e.g. [[1,4,0], [2,2,0],[1,5,0]] indicate that user 1 has no
-                           interest in item 4 and item 5, user 2 has no interest in item 2.
-        :param kg_lines: The Knowledge Graph Lines, is a list of strings. Each element in the list is a
-                         string representing one relation in the Knowledge Graph. The string can be split
-                         into 3 parts by '\t', the first part is head entity, the second part is relation
-                         type, and the third part is tail entity. E.g. ["749\tfilm.film.writer\t2347"]
-                         represent a Knowledge Graph only has one relation, in that relation, head entity
-                         is 749, tail entity is 2347, and the relation type is "film.film.writer".
+        while n is the number of positive samples, and in each sample, the first number represent the user, the second represent the item, and the last indicate interest or not. e.g. [[1,4,0], [2,2,0],[1,5,0]] indicate that user 1 has no interest in item 4 and item 5, user 2 has no interest in item 2.
+        :param kg_lines: The Knowledge Graph Lines, is a list of strings. Each element in the list is a string representing one relation in the Knowledge Graph. The string can be split into 3 parts by '\t', the first part is head entity, the second part is relation type, and the third part is tail entity. E.g. ["749\tfilm.film.writer\t2347"] represent a Knowledge Graph only has one relation, in that relation, head entity is 749, tail entity is 2347, and the relation type is "film.film.writer".
         """
         # Change the code work directory to the root dir of our submit folder
         os.chdir(os.path.dirname(os.path.abspath(__file__)))
-        config = {"batch_size": 256, "eval_batch_size": 1024, "neg_rate": 1.5, "emb_dim": 16, "l1": True, "margin": 30,
-                  "learning_rate": 2e-3, "weight_decay": 5e-4, "epoch_num": 35}
+        config = {
+            "batch_size": 256,
+            "eval_batch_size": 1024,
+            "neg_rate": 3,
+            "emb_dim": 128,
+            "l1": True,
+            "margin": 10,
+            "learning_rate": 0.001,
+            "weight_decay": 1e-4,
+            "epoch_num":150
+}
         self.batch_size = config["batch_size"]
         self.eval_batch_size = config["eval_batch_size"]
         self.neg_rate = config["neg_rate"]
@@ -251,12 +276,26 @@ class KGRS:
         self.epoch_num = config["epoch_num"]
         self.device_index = -1
         self.kg = kg_lines
-        self.dataloader = Dataloader(train_pos, train_neg, self.kg, neg_rate=self.neg_rate,
-                                     train_batch_size=self.batch_size)
-        self.model = TransE(ent_num=self.dataloader.ent_num, rel_num=self.dataloader.rel_num,
-                            dataloader=self.dataloader, margin=self.margin, dim=self.emb_dim, l1=self.l1,
-                            learning_rate=self.learning_rate, weight_decay=self.weight_decay,
-                            device_index=self.device_index)
+        self.dataloader = Dataloader(
+            train_pos,
+            train_neg,
+            self.kg,
+            n_user=n_user,
+            n_item=n_item,
+            neg_rate=self.neg_rate,
+            train_batch_size=self.batch_size,
+        )
+        self.model = TransE(
+            ent_num=self.dataloader.ent_num,
+            rel_num=self.dataloader.rel_num,
+            dataloader=self.dataloader,
+            margin=self.margin,
+            dim=self.emb_dim,
+            l1=self.l1,
+            learning_rate=self.learning_rate,
+            weight_decay=self.weight_decay,
+            device_index=self.device_index,
+        )
 
     def training(self):
         """
@@ -268,16 +307,8 @@ class KGRS:
     def eval_ctr(self, test_data: np.array) -> np.array:
         """
         Evaluate the CTR Task result
-        :param test_data: The test data that you need to predict. The data is a numpy matrix with shape (n, 2),
-                          while `n` is the number of the test samples, and in each sample, the first dimension
-                          is the user and the second is the item. e.g. [[2, 4], [2, 6], [4, 1]] means you need
-                          to predict the interest level of: from user 2 to item 4, from user 2 to item 6, and
-                          from user 4 to item 1.
-        :return: The prediction result, is an n dimension numpy array, and the i-th dimension means the predicted
-                 interest level of the i-th sample, while the higher score means user has higher interest in the
-                 item. e.g. while test_data=[[2, 4], [2, 6], [4, 1]], the return value [1.2, 3.3, 0.7] means that
-                 the interest level from user 2 to item 6 is highest in these samples, and interest level from
-                 user 2 to item 4 is second highest, interest level from user 4 to item 1 is lowest.
+        :param test_data: The test data that you need to predict. The data is a numpy matrix with shape (n, 2), while n is the number of the test samples, and in each sample, the first dimension is the user and the second is the item. e.g. [[2, 4], [2, 6], [4, 1]] means you need to predict the interest level of: from user 2 to item 4, from user 2 to item 6, and from user 4 to item 1.
+        :return: The prediction result, is an n dimension numpy array, and the i-th dimension means the predicted interest level of the i-th sample, while the higher score means user has higher interest in the item. e.g. while test_data=[[2, 4], [2, 6], [4, 1]], the return value [1.2, 3.3, 0.7] means that the interest level from user 2 to item 6 is highest in these samples, and interest level from user 2 to item 4 is second highest, interest level from user 4 to item 1 is lowest.
         """
         eval_batches = np.array_split(test_data, len(test_data) // self.eval_batch_size)
         return self.model.ctr_eval(eval_batches)
@@ -285,14 +316,8 @@ class KGRS:
     def eval_topk(self, users: List[int], k: int = 5) -> List[List[int]]:
         """
         Evaluate the Top-K Recommendation Task result
-        :param users: The list of the id of the users that need to be recommended items. e.g. [2, 4, 8] means
-                      you need to recommend k items for the user 2, 4, 8 respectively, and the term of the user
-                      and recommended item cannot have appeared in the train_pos data.
+        :param users: The list of the id of the users that need to be recommended items. e.g. [2, 4, 8] means you need to recommend k items for the user 2, 4, 8 respectively, and the term of the user and recommended item cannot have appeared in the train_pos data.
         :param k: The number of the items recommended to each user. In this project, k=5.
-        :return: The items recommended to the users respectively, and the order of the items should be sorted by
-                 the interest level of the user to the item. e.g. while user=[2, 4, 8] and k=5, the return value
-                 is [[2, 5, 7, 4, 6],[3, 5, 2, 1, 21],[12, 43, 7, 3, 2]] means you will recommend item 2, 5, 7,
-                 4, 6 to user 2, recommend item 3, 5, 2, 1, 21 to user 4, and recommend item 12, 43, 7, 3, 2 to
-                 user 8, and the interest level from user to the item in the recommend list are degressive.
+        :return: The items recommended to the users respectively, and the order of the items should be sorted by the interest level of the user to the item. e.g. while user=[2, 4, 8] and k=5, the return value is [[2, 5, 7, 4, 6],[3, 5, 2, 1, 21],[12, 43, 7, 3, 2]] means you will recommend item 2, 5, 7, 4, 6 to user 2, recommend item 3, 5, 2, 1, 21 to user 4, and recommend item 12, 43, 7, 3, 2 to user 8, and the interest level from user to the item in the recommend list are degressive.
         """
         return self.model.top_k_eval(users, k=k)
--- eval/evaluate.py
+++ eval/evaluate.py
@@ -1,15 +1,17 @@
 import logging
 import time
 from copy import deepcopy
-
 import math
 import numpy as np
 import torch
 from pytorch_lightning import seed_everything
 from sklearn.metrics import roc_auc_score
+import os
+import sys
 
+# os.path.join(..., '..') -> go up one level to (.../KG-BASED_RECOMM...)
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from demo.kgrs import KGRS
-
 
 
 def nDCG(sorted_items, pos_item, train_pos_item, k=5):
@@ -17,6 +19,7 @@ def nDCG(sorted_items, pos_item, train_pos_item, k=5):
     train_pos_item = set(train_pos_item)
     filter_item = set(filter(lambda item: item not in train_pos_item, pos_item))
     max_correct = min(len(filter_item), k)
+
     train_hit_num = 0
     valid_num = 0
     recommended_items = set()
@@ -25,31 +28,50 @@ def nDCG(sorted_items, pos_item, train_pos_item, k=5):
             train_hit_num += 1
         else:
             valid_num += 1
-            if sorted_items[index] in filter_item and sorted_items[index] not in recommended_items:
-                dcg += 1 / math.log2(index - train_hit_num + 2)  # Rank starts from 0
-                recommended_items.add(sorted_items[index])
-            if valid_num >= k:
-                break
+
+        if (sorted_items[index] in filter_item) and (sorted_items[index] not in recommended_items):
+            # Rank starts from 0
+            dcg += 1 / math.log2(index - train_hit_num + 2)
+            recommended_items.add(sorted_items[index])
+
+        if valid_num >= k:
+            break
+
     idcg = sum([1 / math.log2(i + 2) for i in range(max_correct)])
     return dcg / idcg
 
 
 def load_data():
-    train_pos, train_neg = np.load("../data/train_pos.npy"), np.load(
-        "../data/train_neg.npy")
-    train_pos_len, train_neg_len = int(len(train_pos)*0.8), int(len(train_neg)*0.8)
-    test_pos, test_neg = train_pos[train_pos_len:], train_neg[train_neg_len:]
-    train_pos, train_neg = train_pos[:train_pos_len], train_neg[:train_neg_len]
-    return train_pos, train_neg, test_pos, test_neg
+    train_pos = np.load("data/train_pos.npy")
+    train_neg = np.load("data/train_neg.npy")
+    np.random.shuffle(train_pos)
+    np.random.shuffle(train_neg)
+
+    all_users = set(train_pos[:, 0]) | set(train_neg[:, 0])
+    all_items = set(train_pos[:, 1]) | set(train_neg[:, 1])
+    n_user = max(all_users) + 1
+    n_item = max(all_items) + 1
+
+    train_pos_len = int(len(train_pos) * 0.8)
+    train_neg_len = int(len(train_neg) * 0.8)
+    test_pos = train_pos[train_pos_len:]
+    test_neg = train_neg[train_neg_len:]
+    train_pos = train_pos[:train_pos_len]
+    train_neg = train_neg[:train_neg_len]
+
+    return train_pos, train_neg, test_pos, test_neg, n_user, n_item
 
 
 def get_user_pos_items(train_pos, test_pos):
-    user_pos_items, user_train_pos_items = {}, {}
+    user_pos_items = {}
+    user_train_pos_items = {}
+
     for record in train_pos:
         user, item = record[0], record[1]
         if user not in user_train_pos_items:
             user_train_pos_items[user] = set()
         user_train_pos_items[user].add(item)
+
     for record in test_pos:
         user, item = record[0], record[1]
         if user not in user_train_pos_items:
@@ -57,23 +79,30 @@ def get_user_pos_items(train_pos, test_pos):
         if user not in user_pos_items:
             user_pos_items[user] = set()
         user_pos_items[user].add(item)
+
     return user_pos_items, user_train_pos_items
 
 
 def evaluate():
-    train_pos, train_neg, test_pos, test_neg = load_data()
+    train_pos, train_neg, test_pos, test_neg, n_user, n_item = load_data()
     user_pos_items, user_train_pos_items = get_user_pos_items(train_pos=train_pos, test_pos=test_pos)
+
     logging.disable(logging.INFO)
-    seed_everything(1088, workers=True)
     torch.set_num_threads(8)
+
     auc, ndcg5 = 0, 0
     init_timeout, train_timeout, ctr_timeout, topk_timeout = False, False, False, False
     start_time, init_time, train_time, ctr_time, topk_time = time.time(), 0, 0, 0, 0
-    kgrs = KGRS(train_pos=deepcopy(train_pos),
-                train_neg=deepcopy(train_neg),
-                kg_lines=open('../data/kg.txt', encoding='utf-8').readlines())
-    init_time = time.time() - start_time
 
+    kgrs = KGRS(
+        train_pos=deepcopy(train_pos),
+        train_neg=deepcopy(train_neg),
+        kg_lines=open('data/kg.txt', encoding='utf-8').readlines(),
+        n_user=n_user,
+        n_item=n_item
+    )
+
+    init_time = time.time() - start_time
     kgrs.training()
     train_time = time.time() - start_time - init_time
 
@@ -81,6 +110,7 @@ def evaluate():
     np.random.shuffle(test_data)
     test_label = test_data[:, 2]
     test_data = test_data[:, :2]
+
     kgrs.eval_ctr(test_data)
     scores = kgrs.eval_ctr(test_data=test_data)
     auc = roc_auc_score(y_true=test_label, y_score=scores)
@@ -88,15 +118,55 @@ def evaluate():
 
     users = list(user_pos_items.keys())
     user_item_lists = kgrs.eval_topk(users=users)
-    ndcg5 = np.mean([nDCG(user_item_lists[index], user_pos_items[user], user_train_pos_items[user]) for index, user in
-                     enumerate(users)])
+    ndcg5 = np.mean([
+        nDCG(user_item_lists[index], user_pos_items[user], user_train_pos_items[user])
+        for index, user in enumerate(users)
+    ])
 
     topk_time = time.time() - start_time - init_time - train_time - ctr_time
 
-    return auc, ndcg5, init_timeout, train_timeout, ctr_timeout, topk_timeout, init_time, train_time, ctr_time, topk_time
+    return (
+        auc,
+        ndcg5,
+        init_timeout,
+        train_timeout,
+        ctr_timeout,
+        topk_timeout,
+        init_time,
+        train_time,
+        ctr_time,
+        topk_time,
+    )
 
 
 if __name__ == '__main__':
-    start = time.time()
-    print(evaluate())
-    print(time.time() - start)
+    seed_everything(1088, workers=True)
+    print("Training started...")
+    start_time = time.time()
+
+    # Unpack tuple returned by evaluate
+    (
+        auc,
+        ndcg5,
+        init_timeout,
+        train_timeout,
+        ctr_timeout,
+        topk_timeout,
+        init_time,
+        train_time,
+        ctr_time,
+        topk_time,
+    ) = evaluate()
+
+    # Display the output using formatted string
+    print("\n--- Result ---")
+    print(f"AUC Score : {auc:.4f}")
+    print(f"nDCG@5 Score : {ndcg5:.4f}")
+    print("------------------------")
+    print(f"⏱ Initialization Time: {init_time:.2f} seconds")
+    print(f"⏱ Training Time : {train_time:.2f} seconds")
+    print(f"⏱ CTR Evaluation Time: {ctr_time:.2f} seconds")
+    print(f"⏱ TopK Evaluation Time: {topk_time:.2f} seconds")
+    print("------------------------")
+    total_time = time.time() - start_time
+    print(f"⏱ Execution Time: {total_time:.2f} seconds")
